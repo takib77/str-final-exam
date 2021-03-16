a49b155315583e92c8c554ffaaa6e14de5f6d7e0
Leo <takib77@gmail.com>
2021-03-16 22:13:15 +0100

    Update, save changes

--- src/app/page/user-editor/user-editor.component.ts
+++ src/app/page/user-editor/user-editor.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Observable, of } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 import { User } from 'src/app/model/user';
@@ -20,7 +20,7 @@ export class UserEditorComponent implements OnInit {
    * 2. If the params.id isn't 0: a user from the database based on its id.
    */
   user$: Observable<User> = this.activatedRoute.params.pipe(
-    switchMap( params => {
+    switchMap(params => {
       if (Number(params.id) === 0) {
         return of(new User());
       }
@@ -32,9 +32,20 @@ export class UserEditorComponent implements OnInit {
   constructor(
     private userService: UserService,
     private activatedRoute: ActivatedRoute,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(form: NgForm, user: User): void {
+    if (user.id === 0) {
+      this.userService.createUser(user).subscribe(() => { });
+      this.router.navigate([''])
+    } else {
+      this.userService.updateUser(user).subscribe(() => { });
+      this.router.navigate(['']);
+    }
   }
 
 }
